b62b547eb675e7f45d5493885d6338c8569ba0d2
IJoshi1215 <146590787+IJoshi1215@users.noreply.github.com>
2025-12-02 22:55:00 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (FileStream file = new FileStream("log.txt", FileMode.Create))
+        using (StreamWriter sw = new StreamWriter(file))
+        {
+            sw.WriteLine("Hello from I disposable ");
+        }
+
+        using (FileStream file = new FileStream("log.txt", FileMode.Open))
+        using (StreamReader sr = new StreamReader(file))
+        {
+            Console.WriteLine(sr.ReadToEnd());
+        }
+        
+    }
+
+
+    class SafeHandle : IDisposable
+    {
+        private SafeFileHandle handle;
+
+        public SafeHandle(string path)
+        {
+            handle = CreateFile(path, 0x80000000, 1, IntPtr.Zero, 3, 0, IntPtr.Zero);
+        }
+
+        public void Dispose()
+        {
+            handle?.Dispose();
+        }
+        [DllImport("kernel32.dll",SetLastError = true)]
+        static extern SafeFileHandle CreateFile(string lpfilename, uint desiredAccess, uint shareMode, IntPtr securityAttributes, uint creationDisposition, uint flagAttributes, IntPtr TemplateFile);
+     
+    }
+
+}
