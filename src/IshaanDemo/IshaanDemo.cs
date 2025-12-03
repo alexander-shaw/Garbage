ef316a674e458191a1bec74fd6bedb0ebc2aeec0
vijay <vijay.deva@wsu.edu>
2025-12-02 19:36:31 -0800

    added alex's demo

--- src/IshaanDemo/IshaanDemo.cs
+++ src/IshaanDemo/IshaanDemo.cs
@@ -1,7 +1,10 @@
 // <copyright file="IshaanDemo.cs" company="Ishaan">
 // Copyright (c) Ishaan. All rights reserved.
 // </copyright>
-
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
 namespace IshaanDemo;
 
 /// <summary>
@@ -14,8 +17,34 @@ public class IshaanDemo
     /// </summary>
     public static void Run()
     {
-        Console.WriteLine("ISHAAN'S DEMO");
+        using (FileStream file = new FileStream("log.txt", FileMode.Create))
+        using (StreamWriter sw = new StreamWriter(file))
+        {
+            sw.WriteLine("Hello from I disposable ");
+        }
 
-        // TODO: Implement your demo here.
+        using (FileStream file = new FileStream("log.txt", FileMode.Open))
+        using (StreamReader sr = new StreamReader(file))
+        {
+            Console.WriteLine(sr.ReadToEnd());
+        }
+    }
+    class SafeHandle : IDisposable
+    {
+        private SafeFileHandle handle;
+
+        public SafeHandle(string path)
+        {
+            handle = CreateFile(path, 0x80000000, 1, IntPtr.Zero, 3, 0, IntPtr.Zero);
+        }
+
+        public void Dispose()
+        {
+            handle?.Dispose();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern SafeFileHandle CreateFile(string lpfilename, uint desiredAccess, uint shareMode, IntPtr securityAttributes, uint creationDisposition, uint flagAttributes, IntPtr TemplateFile);
+
     }
 }
